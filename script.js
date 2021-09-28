608dc70da025d52e7c2cd5ed0820f25b8ea5778e
Victoria Xu <vyxu@wustl.edu>
2021-09-28 12:53:09 -0500

    Back-to-top button

--- script.js
+++ script.js
@@ -75,3 +75,26 @@ const revealNav = () => {
 };
 
 document.getElementById("navigationOpen").onclick = revealNav;
+
+// back-to-top button
+//Get the button:
+bttbutton = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    bttbutton.style.display = "block";
+  } else {
+    bttbuton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
